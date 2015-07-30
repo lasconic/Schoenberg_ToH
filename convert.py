e8ae6dd412293fc9ee929e1d84df5fa41254f166
lasconic <lasconic@gmail.com>
2015-07-30 20:01:04 +0200

    update with latest mscz

--- convert.py
+++ convert.py
@@ -2,6 +2,7 @@ import os
 from os.path import basename
 from subprocess import call
 
+process = True
 
 mscoreexe="/Applications/MuseScore 2.app/Contents/MacOS/mscore"
 
@@ -14,23 +15,22 @@ outfile.write('<html>\n'
 '<body>\n'
 )
 
-
-
-
-
 for file in os.listdir("mscz"):
     if file.endswith(".mscz"):
         print "Processing %s file..." % file
         filepath = "mscz/" + file
         filename = os.path.splitext(file)[0]
-        call([mscoreexe, filepath, "-o", "images/"+ filename +".png", "-T", "10", "-r", "72" ])
-        call([mscoreexe, filepath, "-o", "sound/"+ filename +".mp3" ])
-        call([mscoreexe, filepath, "-o", "sound/"+ filename +".ogg" ])
-        outfile.write('<h4>Example '+ filename +'</h4>\n')
+        if (process):
+            call([mscoreexe, filepath, "-o", "images/"+ filename +".png", "-T", "10", "-r", "72" ])
+            call([mscoreexe, filepath, "-o", "sound/"+ filename +".mp3" ])
+            # no ogg for the moment
+            #call([mscoreexe, filepath, "-o", "sound/"+ filename +".ogg" ])
+        outfile.write('<h4>Example '+ str(int(filename)) +'</h4>\n')
         outfile.write('    <p>\n')
-        outfile.write('        <audio controls="controls">\n'
-'           <source src="sound/'+ filename +'.ogg" />\n'
+        outfile.write('        <audio preload="none" controls="controls">\n'
+#'           <source src="sound/'+ filename +'.ogg" />\n'
 '           <source src="sound/'+ filename +'.mp3" />\n'
+'           Your browser does not support the audio tag.'
 '        </audio> \n')
         for png in os.listdir("images/"):
             if png.startswith(filename + "-") and png.endswith(".png"):
